1b37659a80bb42cd070782501e92f5229e15407c
milky_way <shin.kagami13@gmail.com>
2024-12-08 18:47:31 +0700

    fix: fixing scroll trigger for about images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,23 +3,24 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
 export default function About() {
-    useGSAP(() => {
-        const clipAnimation = gsap.timeline({
-            scrollTrigger: {
-                trigger: "#clip",
-                start: "center center",
-                end: "+=800 center",
-                scrub: 0.5,
-                pin: true,
-                pinSpacing: true
-            }
-        })
-        clipAnimation.to(".mask-clip-path", {
-            width: "100vh",
-            height: "100vh",
-            borderRadius: 0
-        })
-    })
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col item-center gap-5">
@@ -27,7 +28,7 @@ export default function About() {
           Welcome to Wuthering Archive
         </p>
         <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-            Discover the world of Solaris 3 Here!.
+          Discover the world of Solaris 3 Here!.
         </div>
         <div className="about-subtext">
           <p>
@@ -38,8 +39,8 @@ export default function About() {
       <div className="h-dvh w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
-            src="/images/backdrop.jpg"
-            alt="background"
+            src="images/backdrop.jpg"
+            alt="Background"
             className="absolute left-0 top-0 size-full object-cover"
           />
         </div>
