1a90aff18280a2657dc981bc6837f27e9003d289
milky_way <shin.kagami13@gmail.com>
2024-12-09 22:58:04 +0700

    feat: make a features bento card 1 line

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -18,11 +18,37 @@ const Features: React.FC = () => {
         </p>
         <div className="border-hsla relative mb-8 h-96 w-full overflow-x-hidden rounded-md md:h-[65vh]">
           <BentoCard
-            title="Extraordinary Adventure"
+            title="Explore The World"
             src="videos/features-1.mp4"
             description="Embark on an epic adventure in the vast and mysterious world of Wuthering Waves. Explore stunning landscapes, from futuristic cities to ancient ruins. Encounter unique characters and unravel the secrets of a world on the brink of change. With dynamic combat and deep customization, your journey is sure to be unforgettable."
             link=""
           />
+        </div>
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              title="Lore"
+              src="videos/features-2.mp4"
+              description="Experience a world rich in lore and mystery. Wuthering Waves offers a captivating narrative that shapes every aspect of gameplay. Uncover the secrets of a world on the brink of change and leave your mark on its history."
+              link=""
+            />
+          </div>
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 ms-32 md:ms-0">
+            <BentoCard
+              title="Characters"
+              src="videos/features-3.mp4"
+              description="Build deep connections with your favorite characters. Enjoy heartwarming interactions and unlock hidden stories."
+              link=""
+            />
+          </div>
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 me-14 md:me-0">
+            <BentoCard
+              title="Echo"
+              src="videos/features-5.mp4"
+              description="Become a master of time with Wuthering Waves' deep Echo system. Collect Echo Fragments to level up your Resonance and unlock powerful new abilities. Discover hidden Echo Resonances throughout the world to gain unique buffs and customize your Echo skills to fit your playstyle."
+              link=""
+            />
+          </div>
         </div>
       </div>
     </section>
