689d386d0bd834d599bbbc7d18c1598570e720a0
milky_way <shin.kagami13@gmail.com>
2024-12-12 21:20:51 +0700

    feat: created separated component for HeroDetail

--- src/app/resonators/page.tsx
+++ src/app/resonators/page.tsx
@@ -1,30 +1,15 @@
+"use client";
+import HeroDetail from "@/components/HeroDetail";
 import SelectionCard from "@/components/SelectionCard";
 
 const ResonatorPage: React.FC = () => {
+
   return (
     <div className="h-screen relative">
-      {/* Background Image */}
-      <div className="absolute inset-0 z-0">
-        <img
-          src="images/background.png"
-          alt="Backdrop"
-          className="min-h-screen h-full w-full object-cover opacity-85"
-        />
-      </div>
+      <HeroDetail />
+      <section className="min-h-dvh bg-black w-screen text-blue-50">
 
-      <div className="relative z-20 flex h-full p-14">
-        <div className="grid grid-cols-5 gap-4 w-full">
-          {Array.from({ length: 9 }).map((_, index) => (
-            <SelectionCard
-              key={index}
-              source="/images/male-rover.png"
-              title="Sanhua"
-              description=""
-              link=""
-            />
-          ))}
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/HeroDetail.tsx
@@ -0,0 +1,75 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const HeroDetail: React.FC = () => {
+  useGSAP(() => {
+    // Set initial styles for background
+    gsap.set("#background", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+
+    gsap.from("#background", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#background",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+    gsap.fromTo(
+      "#text-container",
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      },
+      {
+        clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: "#background",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  return (
+    <>
+      {/* Background */}
+      <div className="absolute inset-0 z-0" id="background">
+        <img
+          src="images/background.png"
+          alt="Backdrop"
+          className="h-full w-full object-cover"
+        />
+      </div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 z-10 bg-black opacity-60" />
+
+      {/* Text Container */}
+      <div
+        id="text-container"
+        className="relative z-20 flex flex-col h-full items-center justify-between p-14 text-center text-white"
+      >
+        <h1 className="text-8xl font-bold font-circular mt-52 uppercase">
+          Resonators
+        </h1>
+        <p className="text-lg text-gray-300 mb-10">
+          Resonators are obtainable characters in Wuthering Waves. Individuals
+          with the ability to resonate with certain objects and manipulate their
+          frequencies are known as Resonators.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default HeroDetail;
